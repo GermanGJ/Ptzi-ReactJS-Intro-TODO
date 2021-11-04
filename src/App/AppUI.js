88be774ad25142916b9ee063315717481ff8d52d
Server2GJ <GermanGomezJimenez@gmail.com>
2021-11-03 19:38:13 -0500

    Proyecto finalde curso.

---
+++ src/App/AppUI.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { TodoContext } from "../TodoContext";
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from "../TodoSearch";
+import { TodoList } from "../TodoList";
+import { TodoItem } from "../TodoItem";
+import { CreateTodoButton } from "../CreateTodoButton";
+import { Modal } from "../Modal";
+import { TodoForm } from "../TodoForm";
+
+
+function AppUI() 
+{
+    const {
+        error, 
+        loading, 
+        searchedTodos, 
+        completeTodo, 
+        deleteTodo,
+        openModal,
+        setOpenModal,
+      } = React.useContext(TodoContext);
+  
+  
+    return (
+      <React.Fragment>
+        <TodoCounter />
+        <TodoSearch />
+               
+        <TodoList>
+            {error && <p>Desesperate, hubo un error...</p>}
+            {loading && <p>Estamos cargando, no desesperes...</p>}
+            {(!loading && !searchedTodos.length) && <p>Crea tu primer Todo</p>}
+            {searchedTodos.map(todo => (
+              <TodoItem 
+                key={todo.text} 
+                text={todo.text} 
+                completed={todo.completed} 
+                onCompleted={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            ))}
+        </TodoList>
+
+        {!!openModal && (
+          <Modal>
+            <TodoForm></TodoForm>
+          </Modal>
+        )}
+
+        <CreateTodoButton
+          setOpenModal={setOpenModal}
+        />  
+      </React.Fragment>
+    );
+}
+
+export {
+    AppUI,
+}
---
+++ src/TodoList/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './TodoList.css';
+
+
+
+function TodoList(props) {
+    return (
+        <section>
+            <ul>
+                {props.children}
+            </ul>
+        </section>
+    );
+}
+
+export {
+    TodoList,
+}
